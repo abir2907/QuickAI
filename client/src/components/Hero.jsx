71f5a0876e6984fa9f4048f8544d2765215e0004
Abir Chodha <abirchodha1@gmail.com>
2025-08-29 18:36:19 +0530

    Fix bug

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -30,8 +30,8 @@ const Hero = () => {
         </button>
       </div>
       <div className="flex items-center gap-4 mt-8 mx-auto text-gray-600">
-        <img src={assets.user_group} alt="" className="h-8" /> Trusted by 10k+
-        people
+        <img src={assets.user_group} alt="users" className="h-8" /> Trusted by
+        10k+ people
       </div>
     </div>
   );
