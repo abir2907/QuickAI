27b847268b78d57c6706aa47615309731c890d8d
Abir Chodha <abirchodha1@gmail.com>
2025-09-07 06:05:19 +0530

    Complete RemoveBackground page and fix bugs in server

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -148,7 +148,7 @@ export const generateImage = async (req, res) => {
 export const removeImageBackground = async (req, res) => {
   try {
     const { userId } = req.auth();
-    const { image } = req.file;
+    const image = req.file;
     const plan = req.plan;
 
     if (plan !== "premium") {
@@ -167,7 +167,7 @@ export const removeImageBackground = async (req, res) => {
       ],
     });
 
-    await sql` INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId}, Remove background from image, ${secure_url}, 'image')`;
+    await sql` INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId}, 'Remove background from image', ${secure_url}, 'image')`;
 
     res.json({ success: true, content: secure_url });
   } catch (error) {
